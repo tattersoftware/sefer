04bbf1d7341e2fb7d019425e5c0cd1f8e031bc85
Matt Gatner <mgatner@tattersoftware.com>
2019-09-30 02:24:30 +0000

    Switch to HTML inputs with set_value

--- app/Views/books/form.php
+++ app/Views/books/form.php
@@ -2,23 +2,20 @@
 	<?= form_open(isset($book) ? "books/{$book->id}" : 'books') ?>
 
 		<div class="form-group">
-			<?= form_label('Book title', 'title') ?>
-			
-			<?= form_input('title', set_value('title', $book->title ?? '', false), 'class="form-control" id="title" placeholder="Enter title"') ?>
+			<label for="title">Book title</label>
+			<input name="title" type="text" value="<?= set_value('title', $book->title ?? '') ?>" class="form-control" id="title" placeholder="Enter title" />
 		</div>
 		
 		<div class="form-group">
-			<?= form_label('ISBN #', 'isbn') ?>
-			
-			<?= form_input('isbn', set_value('title', $book->isbn ?? '', false), 'class="form-control" id="isbn" placeholder="Enter ISBN"') ?>
+			<label for="isbn">ISBN #</label>
+			<input name="isbn" type="text" value="<?= set_value('isbn', $book->isbn ?? '') ?>" class="form-control" id="isbn" placeholder="Enter ISBN" />			
 		</div>
 		
 		<div class="form-group">
-			<?= form_label('Slug', 'slug') ?>
-			
-			<?= form_input('slug', set_value('title', $book->slug ?? '', false), 'class="form-control" id="slug" placeholder="Enter slug shorthand"') ?>
+			<label for="slug">Slug</label>
+			<input name="slug" type="text" value="<?= set_value('slug', $book->slug ?? '') ?>" class="form-control" id="slug" placeholder="Enter slug shorthand" />			
 		</div>
 	
-		<?= form_submit('submit', isset($book) ? 'Update' : 'Create', 'class="btn btn-primary"') ?>
+		<button class="btn btn-primary" type="submit"><?= isset($book) ? 'Update' : 'Create' ?></button>
 	
 	<?= form_close() ?>
