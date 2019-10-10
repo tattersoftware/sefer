ffb7a52f7cf6b93a418ce1aaacf6eb62ceac9dd0
Matt Gatner <mgatner@tattersoftware.com>
2019-10-10 01:44:31 +0000

    Prune API routes

--- app/Controllers/API/Books.php
+++ app/Controllers/API/Books.php
@@ -1,6 +1,5 @@
 <?php namespace App\Controllers\API;
 
-
 class Books extends BaseController
 {	
 	protected $modelName = 'App\Models\BookModel';
@@ -9,18 +8,6 @@ class Books extends BaseController
 	public function index()
 	{
 		return $this->respond($this->model->findAll());
-	}
-	
-	public function new()
-	{
-		// API forms are for AJAX only
-		if (! $this->request->isAJAX())
-		{
-			return parent::new();
-		}
-
-		helper('form');
-		return view('books/form');
 	}
 	
 	public function create()
@@ -34,26 +21,6 @@ class Books extends BaseController
 		
 		$book = $this->model->find($id);
 		return $this->respondCreated(null, lang('Books.created', [$book->title]));
-	}
-	
-	public function edit($id = null)
-	{
-		// API forms are for AJAX only
-		if (! $this->request->isAJAX())
-		{
-			return parent::edit();
-		}
-		
-		$book = $this->model->find($id);
-		if (empty($book))
-		{
-			return $this->respond(null, $this->codes['resource_not_found'], lang('Books.notFound'));
-		}
-		
-		helper('form');
-		$data = ['book' => $book];
-
-		return view('books/form', $data);
 	}
 	
 	public function update($id = null)
